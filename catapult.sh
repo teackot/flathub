efba5dad251c1203341574ef68366da66bffad74
teackot <k.qovekt@gmail.com>
2022-11-13 17:42:31 +0300

    Working global settings; using XDG_CONFIG_HOME and XDG_DATA_HOME to store settings and game data

--- catapult.sh
+++ catapult.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-catapult --data_dir_abs=$HOME/.catapult
+mkdir -p $XDG_CONFIG_HOME/catapult
+mkdir -p $XDG_DATA_HOME/catapult
+cp -n /app/etc/catapult_settings.json $XDG_CONFIG_HOME/catapult/catapult_settings.json
+catapult
+
